8d09b13467b2d6412a1221fe8fa9ec6c5ddb496b
pututwibowo <putut.info@gmail.com>
2024-02-16 08:36:53 +0700

    menambahkan navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layout')
 
 @section('content')
-<p class="text-red-700 text-4xl text-center">Hello World!</p>
+<div class="border-b-2">
+  <header class=" flex px-8 py-3">
+    <h1 class="me-auto text-3xl font-bold">
+      <a href="">Putut Wibowo</a>
+    </h1>
+    <nav class="flex items-center gap-6 me-5 text-lg">
+      <a href="">Garden</a>
+      <a href="">Apps</a>
+      <a href="">About Me</a>
+    </nav>
+  </header>
+</div>
+
 @endsection
